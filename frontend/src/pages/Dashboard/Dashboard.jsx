d6811de45aef2200ae2cfffb824f1274128731ed
Ramya07-cs <rakshanaramesh2020@gmail.com>
2026-08-09 23:30:54 +0530

    feat : add animated bg for dashboard and readme form pages - The bg-colors for glassmorphism can be changed easily by just changing the root variables if needed

---
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./Dashboard.css"
+import ProfileCard from "../../components/ProfileCard/ProfileCard";
+
+export default function Dashboard({data}){
+
+    // const [formData,setFormData] = useState(null);
+
+    return(
+        <>
+            <div className="dashboard-bg">
+                <div className="bg-layer"></div>
+                <div className="content-layer">
+                    <ProfileCard profile = {data.profile}/> 
+                </div>  
+            </div>
+        </>
+    );
+
+}
